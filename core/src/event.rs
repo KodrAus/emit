377b60bf2d7aa63e81551100a5b79402579ad765
KodrAus <kodraus@hey.com>
2024-05-07 07:30:25 +1000

    start work on core docs

--- core/src/event.rs
+++ core/src/event.rs
@@ -1,3 +1,11 @@
+/*!
+The [`Event`] type.
+
+Events are the timestamped combination of [`Template`] and [`Props`] that describe what the event was and any ambient state surrounding it.
+
+Events are a generic abstraction. They only directly define the concepts that are common to all diagnostics. Specific kinds of diagnostic events, like logs, traces, or metric samples, are modeled on top of events using [crate::well_known] key-values in their props.
+*/
+
 use core::{fmt, ops::ControlFlow};
 
 use crate::{
@@ -8,6 +16,9 @@ use crate::{
     timestamp::Timestamp,
 };
 
+/**
+A captured record of something significant that occurred during the operation of a system.
+*/
 #[derive(Clone)]
 pub struct Event<'a, P> {
     // "where"
@@ -21,6 +32,16 @@ pub struct Event<'a, P> {
 }
 
 impl<'a, P> Event<'a, P> {
+    /**
+    Construct an event from its parts.
+
+    Events are composed of:
+
+    - `module`: A [`Path`] to the module that produced the event. This will typically be a [`module_path!`].
+    - `extent`: The [`Extent`] of the event. This is the point in time at which it occurred, or the span of time for which it was active.
+    - `tpl`: The [`Template`] of the event. This is the user-facing description of the event that can be rendered into a readable form.
+    - `props`: The [`Props`] attached to the event, captured from the surrounding environment.
+    */
     pub fn new(
         module: impl Into<Path<'a>>,
         extent: impl ToExtent,
@@ -35,28 +56,52 @@ impl<'a, P> Event<'a, P> {
         }
     }
 
+    /**
+    Get a reference to the module that produced the event.
+    */
     pub fn module(&self) -> &Path<'a> {
         &self.module
     }
 
+    /**
+    Set the module of the event, returning a new one.
+    */
     pub fn with_module(mut self, module: impl Into<Path<'a>>) -> Self {
         self.module = module.into();
         self
     }
 
+    /**
+    Get a reference to the extent of the event, if there is one.
+
+    An event won't have an extent if it was never constructed with one. This can happen in environments without access to a realtime clock.
+    */
     pub fn extent(&self) -> Option<&Extent> {
         self.extent.as_ref()
     }
 
+    /**
+    Set the extent of the event, returning a new one.
+    */
     pub fn with_extent(mut self, extent: impl ToExtent) -> Self {
         self.extent = extent.to_extent();
         self
     }
 
+    /**
+    Get the extent of the event as a point in time.
+
+    If the event has an extent then this method will return `Some`, with the result of [`Extent::as_point`]. If the event doesn't have an extent then this method will return `None`.
+    */
     pub fn ts(&self) -> Option<&Timestamp> {
         self.extent.as_ref().map(|extent| extent.as_point())
     }
 
+    /**
+    Get the start point of the extent of the event.
+
+    If the event has an extent, and that extent covers a span of time then this method will return `Some`. Otherwise this method will return `None`.
+    */
     pub fn ts_start(&self) -> Option<&Timestamp> {
         self.extent
             .as_ref()
@@ -64,19 +109,31 @@ impl<'a, P> Event<'a, P> {
             .map(|span| &span.start)
     }
 
+    /**
+    Get a reference to the template of the event.
+    */
     pub fn tpl(&self) -> &Template<'a> {
         &self.tpl
     }
 
+    /**
+    Set the template of the event, returning a new one.
+    */
     pub fn with_tpl(mut self, tpl: impl Into<Template<'a>>) -> Self {
         self.tpl = tpl.into();
         self
     }
 
+    /**
+    Get a reference to the properties of the event.
+    */
     pub fn props(&self) -> &P {
         &self.props
     }
 
+    /**
+    Set the properties of the event, returning a new one.
+    */
     pub fn with_props<U>(self, props: U) -> Event<'a, U> {
         Event {
             module: self.module,
@@ -86,6 +143,9 @@ impl<'a, P> Event<'a, P> {
         }
     }
 
+    /**
+    Map the properties of the event, returning a new one.
+    */
     pub fn map_props<U>(self, map: impl FnOnce(P) -> U) -> Event<'a, U> {
         Event {
             module: self.module,
@@ -97,10 +157,16 @@ impl<'a, P> Event<'a, P> {
 }
 
 impl<'a, P: Props> Event<'a, P> {
+    /**
+    Get a lazily-evaluated formatting of the event's template.
+    */
     pub fn msg(&self) -> Render<&P> {
         self.tpl.render(&self.props)
     }
 
+    /**
+    Get a new event, borrowing data from this one.
+    */
     pub fn by_ref<'b>(&'b self) -> Event<'b, ByRef<'b, P>> {
         Event {
             module: self.module.by_ref(),
@@ -110,6 +176,9 @@ impl<'a, P: Props> Event<'a, P> {
         }
     }
 
+    /**
+    Get a type-erased event, borrowing data from this one.
+    */
     pub fn erase<'b>(&'b self) -> Event<'b, &'b dyn ErasedProps> {
         Event {
             module: self.module.by_ref(),
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,15 @@
+/*!
+A diagnostic framework for Rust applications.
+
+This library is the core API of `emit`, defining the fundamental abstractions used by the higher-level `emit` crate. This library is home to [`event::Event`], `emit`'s model of diagnostic data through with their [`template::Template`], [`props::Props`], and [`extent::Extent`].
+
+In this library is also the all-encapsulating [`runtime::Runtime`], which collects the platform capabilities and event processing pipeline into a single value that powers the diagnostics for your applications.
+
+If you're looking to explore and understand `emit`'s API, you can start with [`runtime::Runtime`] and [`event::Event`] and follow their encapsulated types.
+
+If you're looking to use `emit` in an application you can use this library directly, but `emit` itself is recommended.
+*/
+
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
 
 #[cfg(feature = "alloc")]
