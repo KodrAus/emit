41f2a45b0b5ab2330c7c0d2e080da42a37e4be4c
KodrAus <kodraus@hey.com>
2024-06-01 08:52:29 +1000

    document terminal emitter

--- targets/term/src/lib.rs
+++ targets/term/src/lib.rs
@@ -1,3 +1,55 @@
+/*!
+Emit diagnostic events to the console.
+
+This library implements a text-based format that's intended for direct end-user consumption, such as in interactive applications.
+
+# Getting started
+
+Add `emit` and `emit_term` to your `Cargo.toml`:
+
+```toml
+[dependencies.emit]
+version = "0.0.0"
+
+[dependencies.emit_term]
+version = "0.0.0"
+```
+
+Initialize `emit` using `emit_term`:
+
+```
+fn main() {
+    let rt = emit::setup()
+        .emit_to(emit_term::stdout())
+        .init();
+
+    // Your app code goes here
+
+    rt.blocking_flush(std::time::Duration::from_secs(30));
+}
+```
+
+`emit_term` uses a format optimized for human legibility, not for machine processing. You may also want to emit diagnostics to another location, such as OTLP through `emit_otlp` or a rolling file through `emit_file` for processing. You can use [`emit::Setup::and_emit_to`] to combine multiple emitters:
+
+```
+# fn some_other_emitter() -> impl emit::Emitter + Send + Sync + 'static {
+#    emit::emitter::from_fn(|_| {})
+# }
+fn main() {
+    let rt = emit::setup()
+        .emit_to(emit_term::stdout())
+        .and_emit_to(some_other_emitter())
+        .init();
+
+    // Your app code goes here
+
+    rt.blocking_flush(std::time::Duration::from_secs(30));
+}
+```
+*/
+
+#![deny(missing_docs)]
+
 use core::{fmt, str, time::Duration};
 use std::{cell::RefCell, cmp, io::Write, iter};
 
@@ -6,48 +58,62 @@ use emit::well_known::{
 };
 use termcolor::{Buffer, BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
 
+/**
+Get an emitter that writes to `stdout`.
+
+Colors will be used if the terminal supports them.
+*/
 pub fn stdout() -> Stdout {
-    Stdout {
-        writer: BufferWriter::stdout(ColorChoice::Auto),
-    }
+    Stdout::new()
 }
 
+/**
+An emitter that writes to `stdout`.
+
+Use [`stdout`] to construct an emitter and pass the result to [`emit::Setup::emit_to`] to configure `emit` to use it:
+
+```
+fn main() {
+    let rt = emit::setup()
+        .emit_to(emit_term::stdout())
+        .init();
+
+    // Your app code goes here
+
+    rt.blocking_flush(std::time::Duration::from_secs(30));
+}
+```
+*/
 pub struct Stdout {
     writer: BufferWriter,
 }
 
-thread_local! {
-    static STDOUT: RefCell<Option<Buffer>> = RefCell::new(None);
-}
+impl Stdout {
+    /**
+    Get an emitter that writes to `stdout`.
 
-fn with_shared_buf(writer: &BufferWriter, with_buf: impl FnOnce(&BufferWriter, &mut Buffer)) {
-    STDOUT.with(|buf| {
-        match buf.try_borrow_mut() {
-            // If there are no overlapping references then use the cached buffer
-            Ok(mut slot) => {
-                match &mut *slot {
-                    // If there's a cached buffer then clear it and print using it
-                    Some(buf) => {
-                        buf.clear();
-                        with_buf(&writer, buf);
-                    }
-                    // If there's no cached buffer then create one and use it
-                    // It'll be cached for future callers on this thread
-                    None => {
-                        let mut buf = writer.buffer();
-                        with_buf(&writer, &mut buf);
-
-                        *slot = Some(buf);
-                    }
-                }
-            }
-            // If there are overlapping references then just create a
-            // buffer on-demand to use
-            Err(_) => {
-                with_buf(&writer, &mut writer.buffer());
-            }
+    Colors will be used if the terminal supports them.
+    */
+    pub fn new() -> Self {
+        Stdout {
+            writer: BufferWriter::stdout(ColorChoice::Auto),
         }
-    });
+    }
+
+    /**
+    Whether to write using colors.
+
+    By default, colors will be used if the terminal supports them. You can explicitly enable or disable colors using this function. If `colored` is true then colors will always be used. If `colored` is false then colors will never be used.
+    */
+    pub fn colored(mut self, colored: bool) -> Self {
+        if colored {
+            self.writer = BufferWriter::stdout(ColorChoice::Always);
+        } else {
+            self.writer = BufferWriter::stdout(ColorChoice::Never);
+        }
+
+        self
+    }
 }
 
 impl emit::emitter::Emitter for Stdout {
@@ -63,6 +129,112 @@ impl emit::emitter::Emitter for Stdout {
 }
 
 impl emit::runtime::InternalEmitter for Stdout {}
+
+fn print_event(
+    out: &BufferWriter,
+    buf: &mut Buffer,
+    evt: &emit::event::Event<impl emit::props::Props>,
+) {
+    if let Some(span_id) = evt.props().pull::<emit::span::SpanId, _>(KEY_SPAN_ID) {
+        if let Some(trace_id) = evt.props().pull::<emit::span::TraceId, _>(KEY_TRACE_ID) {
+            let trace_id_color = trace_id_color(&trace_id);
+
+            write_fg(buf, "▓", Color::Ansi256(trace_id_color));
+            write_plain(buf, " ");
+            write_plain(buf, hex_slice(&trace_id.to_hex(), 6));
+            write_plain(buf, " ");
+        } else {
+            write_plain(buf, "░      ");
+        }
+
+        let span_id_color = span_id_color(&span_id);
+
+        write_fg(buf, "▓", Color::Ansi256(span_id_color));
+        write_plain(buf, " ");
+        write_plain(buf, hex_slice(&span_id.to_hex(), 4));
+        write_plain(buf, " ");
+    }
+
+    if let Some(extent) = evt.extent() {
+        if extent.is_span() {
+            if let Some(len) = extent.len() {
+                write_timestamp(buf, *extent.as_point());
+                write_plain(buf, " ");
+                write_duration(buf, len);
+            } else {
+                write_timestamp(buf, extent.as_range().start);
+                write_plain(buf, "..");
+                write_timestamp(buf, extent.as_range().end);
+            }
+        } else {
+            write_timestamp(buf, *extent.as_point());
+        }
+
+        write_plain(buf, " ");
+    }
+
+    if let Some(kind) = evt.props().get(KEY_EVENT_KIND) {
+        write_fg(buf, kind, KIND);
+        write_plain(buf, " ");
+    }
+
+    let mut lvl = None;
+    if let Some(level) = evt.props().pull::<emit::Level, _>(KEY_LVL) {
+        lvl = level_color(&level).map(Color::Ansi256);
+
+        try_write_fg(buf, level, lvl);
+        write_plain(buf, " ");
+    }
+
+    write_fg(buf, format_args!("{} ", evt.module()), MODULE);
+
+    let _ = evt.msg().write(Writer { buf });
+    write_plain(buf, "\n");
+
+    if let Some(err) = evt.props().get(KEY_ERR) {
+        if let Some(err) = err.to_borrowed_err() {
+            write_plain(buf, "  ");
+            try_write_fg(buf, "err", lvl);
+            write_plain(buf, format_args!(": {err}\n"));
+
+            for cause in iter::successors(err.source(), |err| (*err).source()) {
+                write_plain(buf, "  ");
+                try_write_fg(buf, "caused by", lvl);
+                write_plain(buf, format_args!(": {cause}\n"));
+            }
+        }
+    }
+
+    if let Some(value) = evt.props().get(KEY_METRIC_VALUE) {
+        let buckets = value.as_f64_sequence();
+
+        if !buckets.is_empty() {
+            write_timeseries(buf, &buckets);
+        }
+    }
+
+    let _ = out.print(&buf);
+}
+
+fn write_timeseries(buf: &mut Buffer, buckets: &[f64]) {
+    const BLOCKS: [&'static str; 7] = ["▁", "▂", "▃", "▄", "▅", "▆", "▇"];
+
+    let mut bucket_min = f64::NAN;
+    let mut bucket_max = -f64::NAN;
+
+    for v in buckets {
+        bucket_min = cmp::min_by(*v, bucket_min, f64::total_cmp);
+        bucket_max = cmp::max_by(*v, bucket_max, f64::total_cmp);
+    }
+
+    for v in buckets {
+        let idx = (((v - bucket_min) / (bucket_max - bucket_min)) * ((BLOCKS.len() - 1) as f64))
+            .ceil() as usize;
+        let _ = buf.write(BLOCKS[idx].as_bytes());
+    }
+
+    let _ = buf.write(b"\n");
+}
 
 fn trace_id_color(trace_id: &emit::span::TraceId) -> u8 {
     let mut hash = 0;
@@ -188,112 +360,6 @@ fn write_duration(buf: &mut Buffer, duration: Duration) {
     write_fg(buf, unit, TEXT);
 }
 
-fn print_event(
-    out: &BufferWriter,
-    buf: &mut Buffer,
-    evt: &emit::event::Event<impl emit::props::Props>,
-) {
-    if let Some(span_id) = evt.props().pull::<emit::span::SpanId, _>(KEY_SPAN_ID) {
-        if let Some(trace_id) = evt.props().pull::<emit::span::TraceId, _>(KEY_TRACE_ID) {
-            let trace_id_color = trace_id_color(&trace_id);
-
-            write_fg(buf, "▓", Color::Ansi256(trace_id_color));
-            write_plain(buf, " ");
-            write_plain(buf, hex_slice(&trace_id.to_hex(), 6));
-            write_plain(buf, " ");
-        } else {
-            write_plain(buf, "░      ");
-        }
-
-        let span_id_color = span_id_color(&span_id);
-
-        write_fg(buf, "▓", Color::Ansi256(span_id_color));
-        write_plain(buf, " ");
-        write_plain(buf, hex_slice(&span_id.to_hex(), 4));
-        write_plain(buf, " ");
-    }
-
-    if let Some(extent) = evt.extent() {
-        if extent.is_span() {
-            if let Some(len) = extent.len() {
-                write_timestamp(buf, *extent.as_point());
-                write_plain(buf, " ");
-                write_duration(buf, len);
-            } else {
-                write_timestamp(buf, extent.as_range().start);
-                write_plain(buf, "..");
-                write_timestamp(buf, extent.as_range().end);
-            }
-        } else {
-            write_timestamp(buf, *extent.as_point());
-        }
-
-        write_plain(buf, " ");
-    }
-
-    if let Some(kind) = evt.props().get(KEY_EVENT_KIND) {
-        write_fg(buf, kind, KIND);
-        write_plain(buf, " ");
-    }
-
-    let mut lvl = None;
-    if let Some(level) = evt.props().pull::<emit::Level, _>(KEY_LVL) {
-        lvl = level_color(&level).map(Color::Ansi256);
-
-        try_write_fg(buf, level, lvl);
-        write_plain(buf, " ");
-    }
-
-    write_fg(buf, format_args!("{} ", evt.module()), MODULE);
-
-    let _ = evt.msg().write(Writer { buf });
-    write_plain(buf, "\n");
-
-    if let Some(err) = evt.props().get(KEY_ERR) {
-        if let Some(err) = err.to_borrowed_err() {
-            write_plain(buf, "  ");
-            try_write_fg(buf, "err", lvl);
-            write_plain(buf, format_args!(": {err}\n"));
-
-            for cause in iter::successors(err.source(), |err| (*err).source()) {
-                write_plain(buf, "  ");
-                try_write_fg(buf, "caused by", lvl);
-                write_plain(buf, format_args!(": {cause}\n"));
-            }
-        }
-    }
-
-    if let Some(value) = evt.props().get(KEY_METRIC_VALUE) {
-        let buckets = value.as_f64_sequence();
-
-        if !buckets.is_empty() {
-            write_timeseries(buf, &buckets);
-        }
-    }
-
-    let _ = out.print(&buf);
-}
-
-fn write_timeseries(buf: &mut Buffer, buckets: &[f64]) {
-    const BLOCKS: [&'static str; 7] = ["▁", "▂", "▃", "▄", "▅", "▆", "▇"];
-
-    let mut bucket_min = f64::NAN;
-    let mut bucket_max = -f64::NAN;
-
-    for v in buckets {
-        bucket_min = cmp::min_by(*v, bucket_min, f64::total_cmp);
-        bucket_max = cmp::max_by(*v, bucket_max, f64::total_cmp);
-    }
-
-    for v in buckets {
-        let idx = (((v - bucket_min) / (bucket_max - bucket_min)) * ((BLOCKS.len() - 1) as f64))
-            .ceil() as usize;
-        let _ = buf.write(BLOCKS[idx].as_bytes());
-    }
-
-    let _ = buf.write(b"\n");
-}
-
 struct Writer<'a> {
     buf: &'a mut Buffer,
 }
@@ -393,4 +459,38 @@ impl<'a> Writer<'a> {
     fn write(&mut self, v: impl fmt::Display, color: Color) {
         write_fg(&mut *self.buf, v, color);
     }
+}
+
+fn with_shared_buf(writer: &BufferWriter, with_buf: impl FnOnce(&BufferWriter, &mut Buffer)) {
+    thread_local! {
+        static STDOUT: RefCell<Option<Buffer>> = RefCell::new(None);
+    }
+
+    STDOUT.with(|buf| {
+        match buf.try_borrow_mut() {
+            // If there are no overlapping references then use the cached buffer
+            Ok(mut slot) => {
+                match &mut *slot {
+                    // If there's a cached buffer then clear it and print using it
+                    Some(buf) => {
+                        buf.clear();
+                        with_buf(&writer, buf);
+                    }
+                    // If there's no cached buffer then create one and use it
+                    // It'll be cached for future callers on this thread
+                    None => {
+                        let mut buf = writer.buffer();
+                        with_buf(&writer, &mut buf);
+
+                        *slot = Some(buf);
+                    }
+                }
+            }
+            // If there are overlapping references then just create a
+            // buffer on-demand to use
+            Err(_) => {
+                with_buf(&writer, &mut writer.buffer());
+            }
+        }
+    });
 }
